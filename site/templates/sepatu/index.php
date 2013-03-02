2e57acb1617b8df12b1a75b6b226bb95e7f84030
Jisse Reitsma <info@yireo.com>
2013-03-02 10:51:52 +0100

    Pending changes

--- site/templates/sepatu/index.php
+++ site/templates/sepatu/index.php
@@ -21,7 +21,7 @@ include_once 'helper.php';
 $helper = new SepatuTemplateHelper($this->params);
 
 // Google Fonts
-$googleFonts = array('Titillium+Web', 'Oxygen');
+$googleFonts = array('Oxygen');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -39,7 +39,7 @@ $googleFonts = array('Titillium+Web', 'Oxygen');
 <?php if($this->countModules('mainmenu or logo')) : ?>
 <div id="menubar-wrapper" class="container-fluid container-body">
     <div id="container-menubar">
-        <div class="row-fluid">
+        <div class="row-fluid row-body">
             <div class="span12">
                 <div style="float:right">
                     <jdoc:include type="modules" name="mainmenu" style="none" />
@@ -53,7 +53,7 @@ $googleFonts = array('Titillium+Web', 'Oxygen');
     
 <?php if ($helper->countModules('top-a') || $helper->countModules('top-b')) : ?>
 <div id="top-wrapper" class="container-fluid container-body">
-    <div class="row-fluid">
+    <div class="row-fluid row-body">
         <div class="span12">
             <?php $positionPrefix = 'top-a'; include 'layout/module_rows.php'; ?>
             <?php $positionPrefix = 'top-b'; include 'layout/module_rows.php'; ?>
@@ -64,7 +64,7 @@ $googleFonts = array('Titillium+Web', 'Oxygen');
 
 <?php if ($this->countModules('top-hero')) : ?>
 <div id="top-hero-wrapper" class="container-fluid container-body">
-    <div class="row-fluid">
+    <div class="row-fluid row-body">
         <div class="span12">
             <div class="container-fluid">
                 <div class="row-fluid">
@@ -82,7 +82,7 @@ $googleFonts = array('Titillium+Web', 'Oxygen');
 
 <?php if ($helper->countModules('top-c') || $helper->countModules('top-d')) : ?>
 <div id="top-wrapper" class="container-fluid container-body">
-    <div class="row-fluid">
+    <div class="row-fluid row-body">
         <div class="span12">
             <?php $positionPrefix = 'top-c'; include 'layout/module_rows.php'; ?>
             <?php $positionPrefix = 'top-d'; include 'layout/module_rows.php'; ?>
@@ -93,7 +93,7 @@ $googleFonts = array('Titillium+Web', 'Oxygen');
 
 
 <div id="content-wrapper" class="container-fluid container-body">
-    <div class="row-fluid">
+    <div class="row-fluid row-body">
         <div class="span12">
             <?php if ($this->countModules('content-hero')) : ?>
             <div class="container-fluid">
@@ -125,7 +125,7 @@ $googleFonts = array('Titillium+Web', 'Oxygen');
 
 <?php if ($helper->countModules('bottom-a') || $helper->countModules('bottom-b')) : ?>
 <div id="bottom-wrapper" class="container-fluid container-body">
-    <div class="row-fluid">
+    <div class="row-fluid row-body">
         <div class="span12">
             <?php $positionPrefix = 'bottom-a'; include 'layout/module_rows.php'; ?>
             <?php $positionPrefix = 'bottom-b'; include 'layout/module_rows.php'; ?>
@@ -136,7 +136,7 @@ $googleFonts = array('Titillium+Web', 'Oxygen');
 
 <?php if ($helper->countModules('bottomcontent-a') || $helper->countModules('bottomcontent-b')) : ?>
 <div id="bottomcontent-wrapper" class="container-fluid container-body">
-    <div class="row-fluid">
+    <div class="row-fluid row-body">
         <div class="span12">
             <?php $positionPrefix = 'bottomcontent-a'; include 'layout/module_rows.php'; ?>
             <?php $positionPrefix = 'bottomcontent-b'; include 'layout/module_rows.php'; ?>
@@ -147,7 +147,7 @@ $googleFonts = array('Titillium+Web', 'Oxygen');
 
 <?php if ($helper->countModules('footer-a') || $helper->countModules('footer-b')) : ?>
 <div id="footer-wrapper" class="container-fluid container-body">
-    <div class="row-fluid">
+    <div class="row-fluid row-body">
         <div class="span12">
             <?php $positionPrefix = 'footer-a'; include 'layout/module_rows.php'; ?>
             <?php $positionPrefix = 'footer-b'; include 'layout/module_rows.php'; ?>
